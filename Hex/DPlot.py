ebf9ecc6ca62660a647a6ccc077de2c030df12ad
Yuvraj Jain <20016119+nutcasev15@users.noreply.github.com>
2025-04-26 16:46:06 +0100

    [HEX] Include Reactivity Calculation in Depletion Simulation

--- Hex/DPlot.py
+++ Hex/DPlot.py
@@ -1,74 +1,90 @@
-# OpenMC 0.13.3 Python Post Processing Script for MF Ratio Depletion Study
+# OpenMC 0.15.2 Python Post Processing Script for MF Ratio Depletion Study
 
 
 ############### Library Imports
 import pickle
 import itertools
-import numpy as np
 import matplotlib.markers as mrk
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fnt
-import matplotlib.ticker as tck
+import pandas as pd
 
 
 ############### Load Results from Pickle File
-Pikname = 'Dep-Ref'
-with open(Pikname + '.pkl', 'rb') as f:
-    ModData = pickle.load(f)
-    dep_sch = pickle.load(f)
-    Mname = pickle.load(f)
-    MF_den_ratio = pickle.load(f)
-    channel_dia = pickle.load(f)
-    hex_dia = pickle.load(f)
-    hex_mass = pickle.load(f)
-    K_EVL = pickle.load(f)
-    K_BOL = pickle.load(f)
-    K_EOL = pickle.load(f)
-    U_burn = pickle.load(f)
-    Coef_BOL = pickle.load(f)
-    Coef_XST = pickle.load(f)
-    Coef_MOL = pickle.load(f)
-    Coef_EOL = pickle.load(f)
+pkl_name = 'Dep-Ref'
+with open(pkl_name + '.pkl', 'rb') as f:
+  mod_data = pickle.load(f)
+  dep_sch = pickle.load(f)
+  mod_name = pickle.load(f)
+  MF_den_ratio = pickle.load(f)
+  channel_dia = pickle.load(f)
+  hex_dia = pickle.load(f)
+  hex_mass = pickle.load(f)
+  K_EVL = pickle.load(f)
+  K_BOL = pickle.load(f)
+  K_EOL = pickle.load(f)
+  U_burn = pickle.load(f)
+  coef_BOL = pickle.load(f)
+  coef_EOL = pickle.load(f)
 
 
-############### Plot Results
+############### Post Process Results
 ####### Define Set of Markers for Plotting
-mrkrs = itertools.cycle(mrk.MarkerStyle.filled_markers)
+marks = itertools.cycle(mrk.MarkerStyle.filled_markers)
 
 ###### Define Set of Line Styles for Plotting
 lines = itertools.cycle(['-', '--', ':', '-.'])
 
-####### Kinf Depletion History and Reactivity Coefficients
-for (ModName, Kinf, BOL, XST, MOL, EOL) in zip(Mname, K_EVL, Coef_BOL, Coef_XST, Coef_MOL, Coef_EOL):
-    fig = plt.figure(layout='constrained')
-    ax = fig.add_subplot(111)
-    ax.set_title('$K_{inf}$ Depletion History for ' + ModName)
-    ax.set_xlabel('Burnup (MWd/kg)')
-    ax.set_ylabel('$K_{inf}$')
-    ax.grid()
+####### Plot Kinf Depletion History
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_title('$K_{inf}$ Depletion History at 250 W/cm')
+ax.set_xlabel('Operation Time (days)')
+ax.set_xscale('log')
+ax.set_ylabel('$K_{inf}$')
+ax.grid()
 
-    # Plot BOL Kinf Data with Error Bars for Each Moderator
-    print(ModName + '\n')
-    print(BOL)
-    print('\n')
-    # ax.errorbar(dep_sch, y=[j[0] for j in Kinf], yerr=[j[1] for j in Kinf])
+for (name, Kinf) in zip(mod_name, K_EVL):
+  # Plot BOL Kinf Data with Error Bars for Each Moderator
+  ax.errorbar(
+    x=dep_sch,
+    y=[j[0] for j in Kinf],
+    yerr=[j[1] for j in Kinf],
+    label=name,
+    marker=next(marks),
+    linestyle=next(lines)
+  )
 
-    # # Finalise and Save BOL Kinf vs MF_vol Plot
-    # fig.savefig(Pikname + '_' + ModName + '_Kinf.pdf', format='pdf')
+# Finalise and Save Kinf vs Operation Time Plot
+ax.legend(loc='lower left')
+fig.tight_layout()
+fig.savefig(pkl_name + '_Kinf.pdf', format='pdf')
 
-    # # Clear Figure and Axes
-    # fig.clear()
-    # ax.clear()
+# Clear Figure and Axes
+fig.clear()
+ax.clear()
 
 
-####### Kinf and Burnup Data Table for Various Moderators
+####### Print Reactivity Coefficients
+for (Name, BOL, EOL) in zip(mod_name, coef_BOL, coef_EOL):
+  print(Name + '\n')
+  print('BOL Temperature Reactivity Coefficients:')
+  print(BOL)
+  print('EOL Temperature Reactivity Coefficients:')
+  print(EOL)
+  print('\n')
+
+####### Tabulate Depletion Summary for All Moderators
 fig = plt.figure(figsize=(6, 1.5))
 ax = fig.add_subplot(111)
 ax.axis('off')
 ax.grid('off')
 
 # Retrieve Optimum Moderator to Fuel Volume Ratios
-MF_vol = ['{:1.2f}'.format(T[1]) for T in ModData]
+MF_vol = ['{:1.2f}'.format(T['MFR']) for T in mod_data]
+
+# Format Optimum Linear Mass Value for Table
+mass = ['{:1.3}'.format(M) for M in hex_mass]
 
 # Retrieve Kinf Values at BOL and EOL for Each Moderator
 Kinf_BOL = ['{:1.3f}'.format(K[0][0]) for K in K_EVL]
@@ -78,27 +94,43 @@ Kinf_EOL = ['{:1.3f}'.format(K[-1][0]) for K in K_EVL]
 K_diff = ['{:1.3f}'.format((K[0][0] - K[-1][0])) for K in K_EVL]
 
 # Format Burnup Values for Table
-Burnup = ['{:1.2f}'.format(B) for B in U_burn]
+burnup = ['{:1.3f}'.format(B) for B in U_burn]
 
-rows = ('Optimum MF Ratio', 'BOL $K_{inf}$', 'EOL $K_{inf}$', '$\\Delta K$', 'Burnup (U at. %)')
+# Assign Row Labels for Table
+rows = (
+  'Optimum Volume Ratio',
+  'Optimum Specific Mass (kg/m)',
+  'BOL $K_{inf}$',
+  'EOL $K_{inf}$',
+  '$\\Delta K$',
+  'Burnup (U at. $\\%$)'
+)
 
-# Remove BeO Results and Assemble Cell Data
-cell_text = [MF_vol[1:], Kinf_BOL[1:], Kinf_EOL[1:], K_diff[1:], Burnup[1:]]
+# Assemble Cell Data
+cell_text = [MF_vol, mass, Kinf_BOL, Kinf_EOL, K_diff, burnup]
+Table_obj = ax.table(
+  cellText=cell_text,
+  rowLabels=rows,
+  colLabels=mod_name,
+  loc='center', cellLoc='center'
+)
 
-Table_obj = ax.table(cellText=cell_text,
-                     rowLabels=rows,
-                     colLabels=Mname[1:],
-                     loc='center', cellLoc='center')
-
+# Format Table Row and Column Labels
 Table_obj.auto_set_font_size(False)
 Table_obj.set_fontsize(5.0)
 for (row, col), cell in Table_obj.get_celld().items():
-    if (row == 0 or col == -1):
-        cell.set_text_props(fontproperties=fnt.FontProperties(weight='bold', size=3.5))
+  if (row == 0 or col == -1):
+    cell.set_text_props(fontproperties=fnt.FontProperties(weight='bold', size=3.5))
 
-# Finalise and Save U Atom % Burnup vs MF_vol
-# fig.legend(loc='outside right upper')
-fig.savefig(Pikname + '_Table.pdf', format='pdf', bbox_inches='tight')
+# Finalise and Save Summary Table
+fig.savefig(pkl_name + '_Table.pdf', format='pdf', bbox_inches='tight')
+
+# Create Dataframe and Save Table as Plain LaTeX
+pd.DataFrame(
+  data=cell_text,
+  index=rows,
+  columns=mod_name
+).to_latex(pkl_name + '_Table.tex')
 
 # Clear Figure and Axes
 fig.clear()
--- Hex/Deplete.py
+++ Hex/Deplete.py
@@ -1,14 +1,14 @@
-# OpenMC 0.15.0 Python Script for MF Ratio Depletion Study
+# OpenMC 0.15.2 Python Script for MF Ratio Depletion Study
 
 
 ############### Library Imports
 import os
+from sys import exit
 import json
 import pickle
 import numpy as np
 import openmc
-import openmc.deplete
-import openmc.material
+from openmc.deplete import Results
 
 
 ############## Import Materials Database Symbols and Lookup Functions
@@ -25,34 +25,37 @@ from CoefCalc import calculate_reactivity_coef
 
 ############### Define Investigation Parameters
 # Define Save File Name
-PklName = 'Dep-Ref'
+pkl_name = 'Dep-Ref'
 
 # Define Maximum Fuel Element Linear Power
-pow = 275 # W / cm
+pow = 250 # W / cm
 
+# Define Temperature Deviation in Percent for Reactivity Calculation
+dev = 15.0
 
-# Define Depletion Schedule upto 45 MWd/kg
-deplete_EOL = 45
-# Setup Schedule to Equilibrate Fission Products
-EQU_sch = np.logspace(-4, -3, 3)
-EQU_steps = np.diff(np.array(EQU_sch), prepend=0).tolist()
+# Store Cumulative Depletion Schedule in Days for Post Processing
+dep_sch = [
+  0.3,         # 8 Hours
+  0.6,
+  1.0,         # Xenon Stabilisation Time
+  3.0,
+  10.0,
+  30.0,        # 1 Month
+  90.0,
+  180.0,
+  365.0,       # 1 Year
+  5.0 * 365.0, # Middle of Life
+  10.0 * 365.0 # EOL at 10 Years
+]
 
-# Setup Schedule to Deplete until MOL
-MOL_sch = np.logspace(-2, np.log10(deplete_EOL / 2), 3)
-MOL_steps = np.diff(np.array(MOL_sch), prepend=EQU_sch[-1]).tolist()
-
-# Setup Schedule to Deplete until EOL
-EOL_sch = np.logspace(np.log10(deplete_EOL * 0.667), np.log10(deplete_EOL), 3)
-EOL_steps = np.diff(np.array(EOL_sch), prepend=MOL_sch[-1]).tolist()
-
-# Store Complete Depletion Schedule for Post Processing
-dep_sch = EQU_sch.tolist() + MOL_sch.tolist() + EOL_sch.tolist()
+# Convert Depletion Schedule to Timesteps for Solver
+dep_steps = np.diff(dep_sch).tolist()
 
 # Define Depletion Chain File Name for OpenMC
 chain = 'chain_casl_pwr'
 
 # Define List to Store Moderator Name from Model Builder Function
-Mname = []
+mod_name = []
 
 # Define List to Store MF Density Ratio from Model Builder Function
 MF_den_ratio = []
@@ -74,24 +77,21 @@ K_EOL = []
 # Define List to Store U Atom Burnup
 U_burn = []
 
-# Define Lists to Store Reactivity Calculation Results
-Coef_BOL = []
-Coef_XST = []
-Coef_MOL = []
-Coef_EOL = []
+# # Define Lists to Store Reactivity Calculation Results
+coef_BOL = []
+coef_EOL = []
 
 
 ############### Define List of Moderator Materials to Simulate
-# Tuple Structure: ID, Optimum MF Volume Ratio
-ModData = [
-    (MATID_BeO,            7.975),
-    (MATID_ZrH,            2.092),
-    (MATID_YH,             1.546),
-    (MATID_MgO_40ZrH,      3.184),
-    (MATID_MgO_40YH,       3.008),
-    (MATID_BeO_MgO_40ZrH,  3.395),
-    (MATID_BeO_MgO_40YH,   3.025),
-    (MATID_CaO_CaH,        2.779)
+# Dictionary Structure: ID, Optimum MF Volume Ratio
+mod_data = [
+    {'ID': MATID_Graphite,       'MFR' : 12.00},
+    {'ID': MATID_ZrH,            'MFR' : 1.775},
+    {'ID': MATID_YH,             'MFR' : 1.247},
+    {'ID': MATID_BeO,            'MFR' : 12.00},
+    {'ID': MATID_MgO_40YH,       'MFR' : 2.603},
+    {'ID': MATID_BeO_MgO_40YH,   'MFR' : 2.708},
+    {'ID': MATID_CaO_CaH,        'MFR' : 2.321}
 ]
 
 ############### Import Serpent Fission Q Data for Depletion Simulations
@@ -100,204 +100,184 @@ with open('serpent_fission_q.json', 'r') as f:
 
 
 ############### Define Suite of Reactivity Coefficient Calculations
-def find_model_coef(model : openmc.Model,
-                    mod : openmc.Material, fuel : openmc.Material, cool : openmc.Material,
-                    percent_change = 10.0):
-    # Define Material Density Updater Function
-    def model_density_updater(model : openmc.Model, deviation, mat : openmc.Material):
-        dens = (1.0 + deviation) * get_materials_by_name(mat.name)[0].get_mass_density()
-        model.update_densities([mat.id], dens, density_units='g/cm3')
-        return dens
+def find_model_coef(builder_output : dict, percent_change : float = 10.0):
+  # Define Temperature Updater Function
+  def model_updater(model : openmc.Model, deviation,
+                    MID : int, MFR : float, TIT : float):
+    # Modify Temperatures and Rebuild Model
+    outlet_temp = (1.0 + deviation) * TIT
+    updated_model = BuildModel(MID=MID, MFR=MFR, TIT=outlet_temp)
 
-    # Define Cell Temperature Updater Function
-    def model_temperature_updater(model : openmc.Model, deviation, mat : openmc.Material):
-        temp = (1.0 + deviation) * mat.temperature
-        model.update_cell_temperatures([mat.name], temp)
+    # Update Cell Temperatures
+    # Modify Only Cells With Calculated Cell Temperatures
+    for cell in updated_model['Model']._cells_by_id.values():
+      cell : openmc.Cell
+      if cell.name != '' and isinstance(cell.temperature, float):
+        model.update_cell_temperatures([cell.name], cell.temperature)
 
-        if mat.name is get_material_by_id(MATID_UN).name:
-            model.update_densities([mat.id], get_UN_Ideal_Dens(temp), density_units='g/cm3')
-        elif mat.name is get_material_by_id(MATID_HeXe).name:
-            model.update_densities([mat.id], get_HeXe_40MM_2MPa_Dens(temp), density_units='g/cm3')
+    # Update Material Nuclide Densities
+    # Nuclide Ratios and Composition are Not Modified
+    for mat in updated_model['Model'].materials:
+      mat : openmc.Material
+      if mat.name != '':
+        model.update_densities([mat.name], mat.density, 'g/cm3')
 
-        return temp
+    return outlet_temp
 
-    # Define Percent Deviation Input Values
-    input_dev = np.arange(start=-percent_change, stop=(percent_change + 1.0), step=percent_change)
-    input_dev /= 100
-    input_dev = input_dev.tolist()
+  # Define Percent Deviation Input Values
+  input_dev = np.arange(
+    start=-percent_change,
+    stop=(percent_change + 1.0),
+    step=percent_change
+  )
+  input_dev /= 100
+  input_dev = input_dev.tolist()
 
-    # Calculate Moderator Reactivity Coefficients
-    original_mod_dens = mod.get_mass_density()
-    original_mod_temp = mod.temperature
-    mod_temp_values, mod_temp_coef = calculate_reactivity_coef(model=model,
-                                                               param_list=input_dev,
-                                                               model_updater=model_temperature_updater,
-                                                               updater_static_args={'mat' : mod},
-                                                               print_output=False)
-    model.update_cell_temperatures([mod.name], original_mod_temp)
-    mod_dens_values, mod_dens_coef = calculate_reactivity_coef(model=model,
-                                                               param_list=input_dev,
-                                                               model_updater=model_density_updater,
-                                                               updater_static_args={'mat' : mod},
-                                                               print_output=False)
-    model.update_densities([mod.id], original_mod_dens, density_units='g/cm3')
+  # Run Reactivity Coefficient Calculation in Memory
+  model_coef = calculate_reactivity_coef(
+    model=builder_output['Model'],
+    param_list=input_dev,
+    model_updater=model_updater,
+    updater_static_args={
+      'MID' : builder_output['Moderator ID'],
+      'MFR' : builder_output['Volume Ratio'],
+      'TIT' : builder_output['Outlet Temperature']
+    },
+    started_initialized=True,
+    print_output=False
+  )
 
+  # Reset Temperatures and Densities in Memory
+  # All Other Model Data Left Unchanged from Input State
+  model_updater(builder_output['Model'],
+    deviation=0.0,
+    MID=builder_output['Moderator ID'],
+    MFR=builder_output['Volume Ratio'],
+    TIT=builder_output['Outlet Temperature']
+  )
 
-    # Calculate Fuel Temperature Reactivity Coefficients
-    original_fuel_temp = fuel.temperature
-    fuel_temp_values, fuel_temp_coef = calculate_reactivity_coef(model=model,
-                                                               param_list=input_dev,
-                                                               model_updater=model_temperature_updater,
-                                                               updater_static_args={'mat' : fuel},
-                                                               print_output=False)
-    model.update_cell_temperatures([fuel.name], original_fuel_temp)
-    model.update_densities([fuel.id], get_UN_Ideal_Dens(original_fuel_temp), density_units='g/cm3')
-
-    # Calculate HeXe Coolant Temperature Reactivity Coefficients
-    original_cool_temp = cool.temperature
-    cool_temp_values, cool_temp_coef = calculate_reactivity_coef(model=model,
-                                                               param_list=input_dev,
-                                                               model_updater=model_temperature_updater,
-                                                               updater_static_args={'mat' : cool},
-                                                               print_output=False)
-    model.update_cell_temperatures([cool.name], original_cool_temp)
-    model.update_densities([cool.id], get_HeXe_40MM_2MPa_Dens(original_cool_temp), density_units='g/cm3')
-
-    return [(mod_dens_values, mod_dens_coef), (mod_temp_values, mod_temp_coef), \
-        (fuel_temp_values, fuel_temp_coef), (cool_temp_values, cool_temp_coef)]
+  return model_coef
 
 
 ############### Run Depletion Routine for Each Moderator
-for Mod in ModData:
+for mod in mod_data:
+  try:
     ######## Call Model Builder Function
-    Model_Data = BuildModel(Mod[0], Mod[1])
+    print('>> Building Model <<')
+    model_data = BuildModel(MID=mod['ID'], MFR=mod['MFR'])
+    model : openmc.Model = model_data['Model']
+    print('>> Moderator: {:s} with Volume Ratio: {:.3f} <<'.format(
+      model_data['Moderator Material'].name,
+      mod['MFR']
+    ))
 
     ######## Clear Run Directory of Previous Results
+    print('>> Clearing Previous Result Files <<')
     os.system('rm -rf *.xml')
     os.system('rm -rf *.h5')
 
-    ####### Override Simulation Settings to Reduce Runtime
-    Model_Data[0].settings.batches = 15
-    Model_Data[0].settings.inactive = 5
-    Model_Data[0].settings.particles = 100
+    ######## Initialise Model in Memory
+    print('>> Setting Up Simulation in Memory <<')
+    model.init_lib(output=False)
 
-    Model_Data[0].init_lib()
+    ######## Calculate Temperature Reactivity Coefficients at BOL
+    print('>> Calculating Reactivity Coefficients at BOL <<')
+    BOL_coef = find_model_coef(model_data, dev)
 
-    BOL_Coef = find_model_coef(Model_Data[0],
-                               Model_Data[6],
-                               Model_Data[7],
-                               Model_Data[8],
-                               30.0)
+    ######## Deplete Model According to Schedule in Memory
+    print('>> Depleting Model in Memory <<')
+    model.deplete(
+      dep_steps,
+      method='celi',
+      operator_kwargs={
+        'chain_file' : chain,
+        'fission_yield_mode' : 'average',
+        'fission_q' : serpent_fission_q,
+        'diff_burnable_mats': True
+      },
+      timestep_units='d',
+      power=pow,
+      output=False
+    )
 
-    Model_Data[0].deplete(EQU_steps,
-                          method='celi',
-                          operator_kwargs={'chain_file' : chain,
-                                           'fission_yield_mode' : 'average',
-                                           'fission_q' : serpent_fission_q,
-                                           'diff_burnable_mats': True},
-                          timestep_units='MWd/kg',
-                          power=pow)
+    ######## Calculate Temperature Reactivity Coefficients after Depletion
+    print('>> Calculating Reactivity Coefficients at EOL <<')
+    EOL_coef = find_model_coef(model_data, dev)
 
-    # Accumulate Initial Uranium Atom Concentrations in All Burnable Materials
-    U_BOL = 0
-    dep_res = openmc.deplete.Results('depletion_results.h5')
-    for mat in dep_res.export_to_materials(burnup_index=0):
-        if mat.depletable == True:
-            U_BOL += dep_res.get_atoms(mat, 'U235', 'atom/cm3', 'a')[1][0]
-            U_BOL += dep_res.get_atoms(mat, 'U238', 'atom/cm3', 'a')[1][0]
-
-    XST_Coef = find_model_coef(Model_Data[0],
-                               Model_Data[6],
-                               Model_Data[7],
-                               Model_Data[8],
-                               30.0)
-
-    Model_Data[0].deplete(MOL_steps,
-                          method='celi',
-                          operator_kwargs={'chain_file' : chain,
-                                           'fission_yield_mode' : 'average',
-                                           'fission_q' : serpent_fission_q,
-                                           'diff_burnable_mats': True},
-                          timestep_units='MWd/kg',
-                          power=pow)
-
-    MOL_Coef = find_model_coef(Model_Data[0],
-                            Model_Data[6],
-                            Model_Data[7],
-                            Model_Data[8],
-                            30.0)
-
-    Model_Data[0].deplete(EOL_steps,
-                          method='celi',
-                          operator_kwargs={'chain_file' : chain,
-                                           'fission_yield_mode' : 'average',
-                                           'fission_q' : serpent_fission_q,
-                                           'diff_burnable_mats': True},
-                          timestep_units='MWd/kg',
-                          power=pow)
-
-    EOL_Coef = find_model_coef(Model_Data[0],
-                               Model_Data[6],
-                               Model_Data[7],
-                               Model_Data[8],
-                               30.0)
-
-    Model_Data[0].finalize_lib()
+    ######## Stop Simulation and Deallocate Memory
+    print('>> Finalising Simulation and Deallocating Memory <<')
+    print('>> Moderator: {:s} with Volume Ratio: {:.3f} <<'.format(
+      model_data['Moderator Material'].name,
+      mod['MFR']
+    ))
+    model.finalize_lib()
+    openmc.reset_auto_ids()
 
     ######## Parse Depletion Calculation Results
-    dep_res = openmc.deplete.Results('depletion_results.h5')
-    kinf = dep_res.get_keff('a')[1].tolist()
+    dep_res = Results('depletion_results.h5')
+    kinf = dep_res.get_keff('d')[1].tolist()
 
     # Calculate and Store Uranium Atom Burnup
     # Accumulate Final Uranium Atom Concentration in All Burnable Materials
+    U_BOL = 0
     U_EOL = 0
-    for mat in dep_res.export_to_materials(burnup_index=-1):
-        if mat.depletable == True:
-            U_EOL += dep_res.get_atoms(mat, 'U235', 'atom/cm3', 'a')[1][-1]
-            U_EOL += dep_res.get_atoms(mat, 'U238', 'atom/cm3', 'a')[1][-1]
+    for mat in dep_res.export_to_materials(-1):
+      if mat.depletable == True:
+        U_BOL += dep_res.get_atoms(mat, 'U235', 'atom/cm3', 'd')[1][0]
+        U_BOL += dep_res.get_atoms(mat, 'U238', 'atom/cm3', 'd')[1][0]
+        U_EOL += dep_res.get_atoms(mat, 'U235', 'atom/cm3', 'd')[1][-1]
+        U_EOL += dep_res.get_atoms(mat, 'U238', 'atom/cm3', 'd')[1][-1]
 
     # Calculate Uranium Atom Burnup with Respect to Initial Atom Concentration
     burn = ((U_BOL - U_EOL) / U_BOL) * 100
+    print('>> Uranium Burnup: {:1.3f} % <<'.format(burn))
+    print('\n')
 
-    ######## Delete Depletion Results Container
+    # Delete Depletion Results Container
     del dep_res
+  except KeyboardInterrupt:
+    ######## Abort Simulation without Saving Results
+    exit()
+  except:
+    ######## Stop Simulation on Error Preserving Existing Results
+    break
 
-    # Store Name of Current Moderator
-    Mname.append(Model_Data[1])
+  # Store Name of Current Moderator
+  mod_name.append(model_data['Moderator Material'].name)
 
-    # Store Moderator to Fuel Density Ratio for Current Moderator
-    MF_den_ratio.append(Model_Data[2])
+  # Store Moderator to Fuel Density Ratio for Current Moderator
+  MF_den_ratio.append(model_data['Density Ratio'])
 
-    # Store Coolant Channel Diameter for Current Moderator
-    channel_dia.append(Model_Data[3])
+  # Store Coolant Channel Diameter for Current Moderator
+  channel_dia.append(model_data['Channel Diameter'])
 
-    # Store Fuel Element Circumscribing Diameter for Current Moderator
-    hex_dia.append(Model_Data[4])
+  # Store Fuel Element Circumscribing Diameter for Current Moderator
+  hex_dia.append(model_data['FE Width'])
 
-    # Store Fuel Element Linear Mass Data for Current Moderator
-    hex_mass.append(Model_Data[5])
+  # Store Fuel Element Linear Mass Data for Current Moderator
+  hex_mass.append(model_data['FE Mass'])
 
-    # Store Kinf Values for Current Moderator
-    K_EVL.append(kinf)
-    K_BOL.append([i[0] for i in kinf])
-    K_EOL.append([i[-1] for i in kinf])
+  # Store Kinf Values for Current Moderator
+  K_EVL.append(kinf)
+  K_BOL.append(kinf[0])
+  K_EOL.append(kinf[-1])
 
-    # Store Uranium Atom Burnup Values for Current Moderator
-    U_burn.append(burn)
+  # Store Uranium Atom Burnup Values for Current Moderator
+  U_burn.append(burn)
 
-    Coef_BOL.append(BOL_Coef)
-    Coef_XST.append(XST_Coef)
-    Coef_MOL.append(MOL_Coef)
-    Coef_EOL.append(EOL_Coef)
+  # Store Temperature Reactivity Coefficients for Current Moderator
+  coef_BOL.append(BOL_coef)
+  coef_EOL.append(EOL_coef)
 
-    del Model_Data
+  # Delete Python Instance of Simulation Data
+  del model_data
 
 
 ############### Save Depletion Calculation Results
-with open(PklName + '.pkl', 'wb') as f:
-    pickle.dump(ModData, f)
+with open(pkl_name + '.pkl', 'wb') as f:
+    pickle.dump(mod_data, f)
     pickle.dump(dep_sch, f)
-    pickle.dump(Mname, f)
+    pickle.dump(mod_name, f)
     pickle.dump(MF_den_ratio, f)
     pickle.dump(channel_dia, f)
     pickle.dump(hex_dia, f)
@@ -306,10 +286,8 @@ with open(PklName + '.pkl', 'wb') as f:
     pickle.dump(K_BOL, f)
     pickle.dump(K_EOL, f)
     pickle.dump(U_burn, f)
-    pickle.dump(Coef_BOL, f)
-    pickle.dump(Coef_XST, f)
-    pickle.dump(Coef_MOL, f)
-    pickle.dump(Coef_EOL, f)
+    pickle.dump(coef_BOL, f)
+    pickle.dump(coef_EOL, f)
 
 
 ############### Cleanup XML and H5 Files
